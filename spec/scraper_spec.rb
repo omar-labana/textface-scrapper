6e2db60a2ba392b18843c9a52e4cad2660161cb3
Omar Labana <omarlabana@outlook.com>
2021-05-18 02:31:12 +0200

    Format docs

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/scraper'
 describe Scraper do
-  let(:scraper) { Scraper.new('https://textfac.es/', 'button.facebtn') do |face_tag|
-    face_tag.attributes['data-clipboard-text'].value
-  end 
-  }
+  let(:scraper) do
+    Scraper.new('https://textfac.es/', 'button.facebtn') do |face_tag|
+      face_tag.attributes['data-clipboard-text'].value
+    end
+  end
   let(:scraper2) { Scraper.new('https://textfac.es/', 'button.facebtn') }
-  
+
   describe '#initialize constructor' do
     it 'Fetches HTML/XML data and maps it according to the passed bloack' do
       expect(scraper.data.class)
@@ -19,7 +20,7 @@ describe Scraper do
 
   describe '#generate_face' do
     it 'Return a random element(text face) from the data array' do
-      expect(scraper.generate_face().class)
+      expect(scraper.generate_face.class)
         .to eql(String)
     end
     it 'Return a random element(text face) from the data array' do
@@ -27,5 +28,4 @@ describe Scraper do
         .to eql(Nokogiri::XML::NodeSet) # Hash equivilant
     end
   end
-
 end
